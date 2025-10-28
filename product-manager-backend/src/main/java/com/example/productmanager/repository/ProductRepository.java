99338532e409e365fff1a9896c4b441bdd5d87c8
Jiening <jieningli01@gmail.com>
2025-10-28 14:16:54 -0400

    add edit on front-end and get list of products by name on back-end

--- product-manager-backend/src/main/java/com/example/productmanager/repository/ProductRepository.java
+++ product-manager-backend/src/main/java/com/example/productmanager/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.example.productmanager.repository;
 
 import com.example.productmanager.model.Product;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	List<Product> findByNameIgnoreCase(String name);
 }
